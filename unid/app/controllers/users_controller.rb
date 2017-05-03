d65e602a44606d5a240e31a443f104e679909e0e
Paraprax <parkerjohnston28@gmail.com>
2017-05-03 15:43:52 -0400

    creating users model, controllers, login and signup forms

---
+++ unid/app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+end
--- unid/app/models/user.rb
+++ unid/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  
+  validates :name, :email, uniqueness: true
+  validates :name, :email, presence: true 
+
 
 end

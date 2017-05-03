cd6dd7f2cc8c50a97289916a3675e80039f53f93
Paraprax <parkerjohnston28@gmail.com>
2017-05-03 16:41:38 -0400

    displaying current user email on main page when logged in and fixing form partials

--- unid/app/controllers/application_controller.rb
+++ unid/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    session[:user_id] && User.find(session[:user_id])
+  end
+
 end

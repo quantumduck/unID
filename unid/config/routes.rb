bfa82396a2ffcaaaa79c02c47aa2455c6f4f4e42
quantumduck <quantum.duck@hotmail.com>
2017-05-03 09:02:42 -0400

    better to use except

--- unid/config/routes.rb
+++ unid/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root 'users#new'
 
-  resources :users, only: %i(new create edit update destroy show) do
-    resources :cards, only: %i(new create edit update destroy)
+  resources :users, except: %i(index) do
+    resources :cards, except: %i(index, show)
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

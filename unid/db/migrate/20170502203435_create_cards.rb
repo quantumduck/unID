46050303bdd20050c1c6e27aee0c4cb162346cd2
quantumduck <quantum.duck@hotmail.com>
2017-05-04 16:35:27 -0400

    drop database and edit migrations

--- unid/db/migrate/20170502203435_create_cards.rb
+++ unid/db/migrate/20170502203435_create_cards.rb
@@ -1,11 +1,12 @@
 class CreateCards < ActiveRecord::Migration[5.0]
   def change
     create_table :cards do |t|
-      t.integer :user_id
+      t.references :user, foreign_key: true
+      t.string :username
       t.string :name
       t.string :url
       t.text :description
-      
+
       t.timestamps
     end
   end

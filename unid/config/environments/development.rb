335c705c9e4fd73c902823eea76e165ec4a6f040
quantumduck <quantum.duck@hotmail.com>
2017-05-04 17:13:57 -0400

    added mailer and account details in development environment

--- unid/config/environments/development.rb
+++ unid/config/environments/development.rb
@@ -29,6 +29,18 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'unidteam',
+    password: 'MailAccountPassword5',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.

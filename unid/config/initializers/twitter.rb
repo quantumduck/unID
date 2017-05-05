a162cd0de8e924c826489e3e8d65981241e32653
Devin Leduc <ixeres@gmail.com>
2017-05-05 17:18:43 -0400

    Figaro + Twitter Initialize working. Currently.

--- unid/config/initializers/twitter.rb
+++ unid/config/initializers/twitter.rb
@@ -1,9 +1,9 @@
-Twitter.require_keys("consumer_key", "consumer_secret", "access_token", "access_token_secret")
-
+Figaro.require_keys("twitter_consumer_key", "twitter_consumer_secret", "twitter_access_token", "twitter_access_token_secret")
+#
 CLIENT = Twitter::REST::Client.new do |config|
     #Make secret values go here:
-    config.consumer_key        = ENV["consumer_key"]
-    config.consumer_secret     = ENV["consumer_secret"]
-    config.access_token        = ENV["access_token"]
-    config.access_token_secret = ENV["access_token_secret"]
+    config.consumer_key        = ENV["twitter_consumer_key"]
+    config.consumer_secret     = ENV["twitter_consumer_secret"]
+    config.access_token        = ENV["twitter_access_token"]
+    config.access_token_secret = ENV["twitter_access_token_secret"]
   end

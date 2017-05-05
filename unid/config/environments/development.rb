4e42900c4401dd5841aea07fb485bed9bbcd9c66
quantumduck <quantum.duck@hotmail.com>
2017-05-05 14:15:33 -0400

    changing to environment variable for email access

--- unid/config/environments/development.rb
+++ unid/config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
     port: 587,
     domain: 'gmail.com',
     user_name: 'unidteam',
-    password: 'MailAccountPassword5',
+    password: ENV['send_mail_password'],
     authentication: 'plain',
     enable_starttls_auto: true
   }

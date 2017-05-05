019b2c5f1cefb6e3223918d40245e9c5a148c247
Devin Leduc <ixeres@gmail.com>
2017-05-05 10:47:17 -0400

    Added twitter initializer to .gitignore, because secrecy.

--- unid/config/initializers/twitter_api.rb
+++ unid/config/initializers/twitter_api.rb
@@ -1,6 +1,6 @@
 # Configure twitter gem with authentication credentials
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key    = "o6WxNKLcDbsfooct2EEhcLKJj"
-  config.consumer_secret = "gjmZSCWTehlgj2VR0zdgpnknol9ufA34XilvGFrRjTFpLQRwhJ"
+  config.consumer_key    = ""
+  config.consumer_secret = ""
 end

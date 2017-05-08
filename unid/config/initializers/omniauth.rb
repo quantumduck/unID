dca16823e828134e0e16ac5916ca1720f26d1ae0
Devin Leduc <ixeres@gmail.com>
2017-05-08 15:59:37 -0400

    Added OmniAuth and OmniAuth-Twitter gem and initializer to facilitate Twitter OAuth handling.

---
+++ unid/config/initializers/omniauth.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, ENV["twitter_consumer_key"], ENV["twitter_consumer_secret"]
+  {
+    :authorize_params => {
+      :force_login => 'true',
+      :use_authorize => 'true'
+    }
+  }
+end

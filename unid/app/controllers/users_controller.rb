2fe1009004ee8613eaafca0ece3a1321d2439702
Paraprax <parkerjohnston28@gmail.com>
2017-05-08 15:24:38 -0400

    adding temp_password column to users table and generation random password if fields left blank on creation

--- unid/app/controllers/users_controller.rb
+++ unid/app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
 
   end
 
   def create
+    @user = User.new(user_params)
+      @user.temp_password = rand.to_s
+      @user.password = @user.temp_password
+    if @user.save
+      redirect_to "/#{@user.username}"
+    else
+      render :new
+    end
+  end
+
+  def change_password
+
   end
 
   def show
@@ -15,6 +28,11 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :username)
   end
 
 end
--- unid/db/schema.rb
+++ unid/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503185145) do
+ActiveRecord::Schema.define(version: 20170508191536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170503185145) do
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "temp_password"
   end
 
   add_foreign_key "profiles", "users"

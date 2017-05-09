c8a5fcf11e4beac296ca3ea70f090bf696adce39
quantumduck <quantum.duck@hotmail.com>
2017-05-09 10:58:26 -0400

    adding gmail smtp settings

--- unid/config/environments/development.rb
+++ unid/config/environments/development.rb
@@ -29,6 +29,17 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'unidteam',
+    password: ENV['send_mail_password'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }

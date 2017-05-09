d51207a305ea53253b7933db9833f92154e0eb33
quantumduck <quantum.duck@hotmail.com>
2017-05-09 16:01:17 -0400

    adding an index on username

--- unid/app/controllers/users_controller.rb
+++ unid/app/controllers/users_controller.rb
@@ -1,34 +1,12 @@
 class UsersController < ApplicationController
 
   def new
-
-    redirect_to new_user_registration_path
-
-  end
-
-  def create
-    @user = User.new(user_params)
-      @user.temp_password = rand(10000000000).to_s
-      @user.password = @user.temp_password
-      @user.password_confirmation = @user.temp_password
-    if @user.save
-      redirect_to "/#{@user.username}/#{@user.temp_password}/change_password"
+    if current_user
+      redirect_to user_path(current_user.username)
     else
-      render :new
+      redirect_to new_user_registration_path
     end
-  end
 
-  def change_password
-    @user = User.find_by(username: params[:id])
-    if @user.temp_password
-      if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-      else
-        redirect_to "/#{@user.username}"
-      end
-    else
-      redirect_to "/#{@user.username}"
-    end
   end
 
   def show
@@ -51,36 +29,8 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user && @user.authenticate(params[:user][:temp_password])
 
-         if @user.update(change_password_params)
-          @user.temp_password = nil
-          @user.save
-           redirect_to "/#{@user.username}"
-         else
-           render :change_password
-         end
 
-    else
-      redirect_to "/#{@user.username}"
-    end
-
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:email, :username)
-  end
-
-  def change_password_params
-    params.require(:user).permit(:email, :username, :password, :password_confirmation)
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:username, :email)
-    end
   end
 
 end
--- unid/db/schema.rb
+++ unid/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509152220) do
+ActiveRecord::Schema.define(version: 20170509195657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170509152220) do
     t.inet     "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
   add_foreign_key "profiles", "users"

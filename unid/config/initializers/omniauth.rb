e141fd46babb1410a8a63ea677a9d876a90c75b4
quantumduck <quantum.duck@hotmail.com>
2017-05-10 09:22:51 -0400

    adding developer strategy option

--- unid/config/initializers/omniauth.rb
+++ unid/config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
   provider :twitter, ENV["twitter_consumer_key"], ENV["twitter_consumer_secret"]
   {
     :authorize_params => {

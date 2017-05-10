252bc6b9d4e5f45480972798556daad059e13f52
quantumduck <quantum.duck@hotmail.com>
2017-05-10 13:44:02 -0400

    adding populating methods to profile controller

--- unid/app/controllers/profiles_controller.rb
+++ unid/app/controllers/profiles_controller.rb
@@ -1,15 +1,29 @@
 class ProfilesController < ApplicationController
+  # before_action :ensure_logged_in
+  skip_before_action :verify_authenticity_token, only: :omniauth_create_post
 
   def new
     @profile = Profile.new
   end
 
   def omniauth_create
-    auth = env['omniauth.auth'].to_hash
+    case params[:provider]
+    when 'twitter'
+      @profile = Profile.new(twitter_params)
+    when 'google_oauth2'
+      @profile = Profile.new(google_params)
+    else
+      @profile = Profile.new(profile_params)
+    end
+    if @profile.save
+      redirect_to "/#{current_user.username}"
+    else
+      redirect_to new_profile_path(curent_user.username)
+    end
   end
 
-  def omniauth_create
-    auth = env['omniauth.auth'].to_hash
+  def omniauth_create_post
+
   end
 
   def create
@@ -31,7 +45,17 @@ class ProfilesController < ApplicationController
 
 private
   def profile_params
-    params.require(:profile).permit(:username, :name, :description, :url, :network)
+    params.require(:profile).permit(:uid, :name, :description, :url, :network)
+  end
+
+  def twitter_params
+    auth = env['omniauth.auth'].to_hash
+    {uid: auth.uid, provider: auth.provider, name: auth.name, nickname: auth.nickname, image: auth.image}
+  end
+
+  def google_params
+    auth = env['omniauth.auth'].to_hash
+    {uid: auth.uid, provider: auth.provider, name: auth.name, email: auth.email, image: auth.image}
   end
 
 end

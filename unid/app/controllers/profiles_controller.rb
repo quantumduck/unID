973466d8e21b4e634fd77b2d6543d8aac74fed29
quantumduck <quantum.duck@hotmail.com>
2017-05-12 11:02:11 -0400

    removing abandoned changes in profiles

--- unid/app/controllers/profiles_controller.rb
+++ unid/app/controllers/profiles_controller.rb
@@ -62,44 +62,8 @@ class ProfilesController < ApplicationController
   def destroy
   end
 
-  def oauth_trigger
-    request = 'https://accounts.google.com/o/oauth2/v2/auth?' +\
-      'redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fuselessendpoint%2F1&' +\
-      'prompt=consent&response_type=code&client_id=' +\
-      ENV['google_client_id'] +\
-      '&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly+' +\
-      'https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly&' +\
-      'access_type=offline'
-    response = HTTParty.get(request)
-    render html: response.body.html_safe
-  end
-
-  def oauth_experiment1
-    code = CGI.escape(params['code']) + '&'
-    uri = 'https://www.googleapis.com/oauth2/v4/token'
-    body = 'code=' + code +\
-      'client_id='+ ENV['google_client_id'] +\
-      '&client_secret=' + ENV['google_client_id_secret'] +\
-      '&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fuselessendpoint%2F1' +\
-      '&scope=&grant_type=authorization_code'
-    headers = {'content-type' => 'application/x-www-form-urlencoded'}
-    response1 = HTTParty.post(uri, body: body, headers: headers).parsed_response
-    token = response1['access_token']
-    uri = 'https://www.googleapis.com/youtube/v3/channels?part=id&mine=true'
-    headers = {'Authorization' => 'Bearer ' + token}
-    # helpme1111
-    response2 = HTTParty.get(uri, headers: headers)
-    render plain: response2.inspect
-  end
-
-  def oauth_experiment2
-
-  end
-
-
-
-
 private
+
   def profile_params
     params.require(:profile).permit(:uid, :name, :description, :url, :network)
   end

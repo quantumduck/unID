7d4fe82937cff844dfe4dc87bfc8fbd8b2841eb3
quantumduck <quantum.duck@hotmail.com>
2017-05-14 19:42:05 -0400

    changing providers controller in preparation for merge

--- unid/app/controllers/providers_controller.rb
+++ unid/app/controllers/providers_controller.rb
@@ -35,21 +35,145 @@ class ProvidersController < ApplicationController
 
 
   def authorize
-    settings = SETTINGS[params[:provider]]
-    scopes = settings[:scopes].map { |s| CGI.escape(s) }
-    scopes = scopes.join('%20')
-    query = '?redirect_uri='
-    query += CGI.escape(settings[:callback]) + '&'
-    settings[:params].each { |k, v| query += k + '=' + v + '&'}
-    query += 'scope=' + scopes + '&'
-    query += 'client_id=' + settings[:client_id]
-    query += '&state=' + settings[:state]
-    response = HTTParty.get(settings[:auth_uri] + query)
-    # render plain: settings[:auth_uri] + query
-    render html: response.body.html_safe
+    # This route is only used for providers without an oauth2 gem.
+    if params[:provider] != 'youtube' || current_user
+      settings = SETTINGS[params[:provider]]
+      scopes = settings[:scopes].map { |s| CGI.escape(settings[:base_uri] + s) }
+      scopes = scopes.join('+')
+      query = '?redirect_uri='
+      query += CGI.escape(settings[:callback]) + '&'
+      settings[:params].each { |k, v| query += k + '=' + v + '&'}
+      query += 'scope=' + scopes + '&'
+      query += 'client_id=' + ENV['google_client_id']
+      response = HTTParty.get(settings[:auth_uri] + query)
+      render html: response.body.html_safe
+    else
+      # New users cannot add youtube accounts
+      redirect_to '/auth/google'
+    end
   end
 
   def callback
+    # IMPORTANT! Do not render views from this route in production code!
+    provider = params[:provider]
+    if provider == 'youtube' || provider == 'google'
+      get_token(provider)
+    else
+      oauth_params = get_params(provider)
+      if current_user
+        # If user is signed in, create a new profile.
+        create_profile(oauth_params)
+      else
+        login_user(oauth_params)
+      end
+    end
+  end
+
+
+private
+
+  def get_params(provider)
+    auth = env['omniauth.auth'].to_hash
+    new_params = {
+      uid: auth['uid'],
+      provider: auth['provider'],
+      name: auth['info']['name'],
+      token: auth['credentials']['token']
+    }
+    case provider
+    when 'twitter'
+      new_params[:nickname] = auth['info']['nickname'],
+      new_params[:image] = auth['info']['image']
+      new_params[:url] = 'https://twitter.com/' + auth['info']['nickname']
+    when 'linkedin'
+      new_params[:first_name] = auth['info']['first_name']
+      new_params[:last_name] = auth['info']['last_name']
+      new_params[:email] = auth['info']['email']
+      new_params[:image] = auth['info']['image']
+      new_params[:description] = auth['info']['headline']
+      new_params[:nickname] = auth['info']['nickname']
+      new_params[:url] = auth['info']['urls']['public_profile']
+    when 'tumblr'
+      new_params[:nickname] = auth['info']['nickname']
+      new_params[:image] = auth['info']['avatar'],
+      new_params[:url] = "http://#{auth['info']['nickname']}.tumblr.com"
+    end
+    new_params
+  end
+
+  def login_user(user_params)
+    profile = Profile.where(provider: user_params[:provider], uid: user_params[:uid]).first
+    if profile
+      user = profile.user
+      session[:user_id] = user.id
+      redirect_to "/#{user.username}", notice: "Logged in via #{profile.provider.capitalize}!"
+    else
+      user = create_user(user_params)
+    end
+  end
+
+  def create_user(user_params)
+    @user = User.new
+    new_password = SecureRandom.random_number(36**12).to_s(36).rjust(12, "0")
+    @user.name = user_params[:name]
+    @user.password = new_password
+    @user.password_confirmation = new_password
+    if user_params[:email]
+      @user.email = user_params[:email]
+      @user.username = user_params[:email].split('@').first
+    elsif user_params[:nickname]
+      @user.username = user_params[:nickname]
+    else
+      @user.username = user_params[:uid]
+    end
+    @user.name = user_params[:name]
+    if @user.save
+      session[:user_id] = user.id
+      redirect_to "/#{@user.username}", notice: "Logged in via #{provider.capitalize}!"
+    else
+      render 'users/new'
+    end
+  end
+
+  def create_profiles(profile_params)
+    profile_params[:multiple].each do |mult|
+      new_params = mult
+      new_params[:uid] = profile_params[:uid]
+      new_params[:name] = profile_params[:name]
+      new_params[:url] = profile_params[:url]
+      new_params[:image] = profile_params[:image]
+      create_profile(new_params)
+    end
+  end
+
+  def create_profile(profile_params)
+    profile =  Profile.where(
+      provider: profile_params[:provider],
+      uid: profile_params[:uid]
+    ).first
+    if profile
+      if profile.update(profile_params)
+        redirect_to "/#{current_user.username}"
+      else
+        # this may need to be changed
+        @profile = profile
+        render 'profiles/edit'
+      end
+    else
+      profile = Profile.new(oauth_params)
+      profile.user_id = current_user.id
+      if profile.save
+        flash[:success] = "successful oauth get request"
+        redirect_to "/#{current_user.username}"
+      else
+        render plain: oauth_params
+      end
+    end
+  end
+
+  #### Google and Youtube ####
+
+  def get_token(provider)
     settings = SETTINGS[params[:provider]]
     uri = settings[:base_uri] + settings[:token_path]
     body = 'code=' + CGI.escape(params[:code]) + \
@@ -59,76 +183,63 @@ class ProvidersController < ApplicationController
       '&scope=&grant_type=authorization_code'
     token_response = HTTParty.post(uri, body: body, headers: settings[:token_headers])
     token_info = token_response.parsed_response
-    profile = Profile.new
     if token_response.code == 200
-      profile.token = token_info['access_token']
-      profile.expires_at = Time.now + token_info['expires_in']
-      profile.expires = true
-      profile.refresh_token = token_info['refresh_token']
-      profile.user_id = current_user.id
-      profile.provider = params[:provider]
-      call_id_api(profile)
+      oauth_params = {
+        token: token_info['access_token'],
+        expires_at: Time.now + token_info['expires_in'],
+        expires: true,
+        refresh_token: token_info['refresh_token'],
+        provider: params[:provider]
+      }
+      call_id_api(oauth_params)
     else
       render plain: "ERROR: no token\n\n#{token_response.inspect}"
     end
-
   end
 
-  def linkedin_callback
-
-    render plain: params.inspect
-
-  end
-
-  def call_id_api(profile)
-    settings = SETTINGS[profile.provider]
+  def call_id_api(oauth_params)
+    settings = SETTINGS[oauth_params[:provider]]
     uri = settings[:base_uri] + settings[:id_query]
     headers = {
-      'Authorization' => 'Bearer ' + profile.token
+      'Authorization' => 'Bearer ' + oauth_params[:token]
     }
     api_response = HTTParty.get(uri, headers: headers)
     if api_response.code == 200
-      case profile.provider
+      case oauth_params[:provider]
       when 'youtube'
-        if api_response.parsed_response['items'].size >= 1
-          channel = api_response.parsed_response['items'][0]
-          profile.uid = channel['id']
-          profile.url = settings[:profile_prefix] + profile.uid
-          profile.name = channel['snippet']['title']
-          profile.image = channel['snippet']['thumbnails']['default']['url']
-          # profile.image = channel['brandingSettings']['image']['bannerImageUrl']
-          if profile.save
-            redirect_to "/#{current_user.username}"
-          else
-            render plain: "ERROR: profile save\n\n#{profile.inspect}"
-          end
+        oauth_params[:multiple] =
+        api_response.parsed_response['items'].map do |channel|
+          {
+            uid: channel['id'],
+            url: settings[:profile_prefix] + channel['id'],
+            name: channel['brandingSettings']['channel']['title'],
+            image: channel['brandingSettings']['image']['bannerImageUrl']
+          }
+        end
+        if oauth_params[:multiple].length > 0
+          create_profiles(oauth_params)
         else
-          render plain: "ERROR: no id\n\n#{api_response.inspect}"
+          render plain: "ERROR: no channels found\n\n#{api_response.inspect}"
         end
 
       when 'google'
-        profile.uid = api_response.parsed_response['id']
-        profile.url = settings[:profile_prefix] + profile.uid
-        profile.name = api_response.parsed_response['displayName']
-        profile.first_name = api_response.parsed_response['givenName']
-        profile.last_name = api_response.parsed_response['familyName']
-        profile.image = api_response.parsed_response['image']['url']
-        if profile.save
-          redirect_to "/#{current_user.username}"
+        oauth_params[:uid] = api_response.parsed_response['id']
+        oauth_params[:url] = settings[:profile_prefix] + oauth_params[:uid]
+        oauth_params[:name] = api_response.parsed_response['displayName']
+        oauth_params[:first_name] = api_response.parsed_response['givenName']
+        oauth_params[:last_name] = api_response.parsed_response['familyName']
+        oauth_params[:image] = api_response.parsed_response['image']['url']
+        if current_user
+          create_profile(oauth_params)
         else
-          render plain: "ERROR: profile save\n\n#{profile.inspect}"
+          login_user(oauth_params)
         end
-      when 'linkedin'
-        render plain: api_response.inspect
-
-
+      else
+        render plain: "ERROR: Unknown Provider\n\n#{api_response.inspect}"
       end
     else
       render plain: "ERROR: #{api_response.code}\n\n#{api_response.inspect}"
     end
   end
-
-private
-
 
 end

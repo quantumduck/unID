dd4c8491676975500ce3c8cbe41969f28b934ed8
Alex <alex.mahinko@gmail.com>
2017-05-15 19:20:03 -0400

    Twitch live bug fully implemented (sorta). Updates delayed and only on refresh

--- unid/app/assets/javascripts/livebug.js
+++ unid/app/assets/javascripts/livebug.js
@@ -1,9 +1,28 @@
 $('document').ready(function() {
 
+var headerUrl = "https://api.twitch.tv/kraken/streams/" + username;
+
+$.ajaxSetup ({
+        cache: false
+    });
 
 
-  $('body').append()
+  $('body').append();
 
-  $.getJSON('https://api.twitch.tv/kraken/streams/' + username )
+ $.ajax({
+   url: headerUrl,
+   method: 'get',
+   response_type: 'jsonp',
+   data: {},
+   headers: {'Client-ID': 'uj53o98rasdwbhwic692sh6jmd36w5'}
+ }).success( function(response) {
+   console.log(response);
+   if (response['stream'] == null) {
+     $('#twitch_card').append("offline")
+   }
+   else {
+     $('#twitch_card').append("LIVE")
+   }
+ });
 
 });
--- unid/app/controllers/providers_controller.rb
+++ unid/app/controllers/providers_controller.rb
@@ -102,10 +102,12 @@ private
       new_params[:image] = auth['info']['image']
       new_params[:url] = 'https://facebook.com/' + auth['uid']
     when 'twitch'
+      puts auth
       new_params[:description] = auth['info']['description']
       new_params[:image] = auth['info']['image']
       new_params[:url] = "https://www.twitch.tv/" + auth["info"]["name"]
       new_params[:expires] = false
+      puts auth
     end
     new_params
   end

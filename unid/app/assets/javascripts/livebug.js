968dd86c1164205f1516ce1bcc6d47c0815d7333
Alex <alex.mahinko@gmail.com>
2017-05-16 11:25:00 -0400

    Livebug for twitch now auto-updates, returning (reasonably) up-to-date live status

--- unid/app/assets/javascripts/livebug.js
+++ unid/app/assets/javascripts/livebug.js
@@ -6,23 +6,32 @@ $.ajaxSetup ({
         cache: false
     });
 
+    var interval
 
-  $('body').append();
+    function livebug() {
 
- $.ajax({
-   url: headerUrl,
-   method: 'get',
-   response_type: 'jsonp',
-   data: {},
-   headers: {'Client-ID': 'uj53o98rasdwbhwic692sh6jmd36w5'}
- }).success( function(response) {
-   console.log(response);
-   if (response['stream'] == null) {
-     $('#twitch_card').append("offline")
-   }
-   else {
-     $('#twitch_card').append("LIVE")
-   }
- });
+
+  $.ajax({
+    url: headerUrl,
+    method: 'get',
+    response_type: 'jsonp',
+    data: {},
+    headers: {'Client-ID': 'uj53o98rasdwbhwic692sh6jmd36w5'}
+  }).success( function(response) {
+    console.log(response);
+    if (response['stream'] == null) {
+      $('#twitch_card').html("offline")
+    }
+    else {
+      $('#twitch_card').html("LIVE")
+    }
+  });
+};
+
+livebug();
+
+interval = setInterval(livebug, 120000);
+
+
 
 });

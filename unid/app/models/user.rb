0334ac45bd6857f1c20f1f21892730ede0d63d03
Devin Leduc <ixeres@gmail.com>
2017-05-16 12:27:04 -0400

    Paths/views correspond to appropriate routes now. Avatars still don't work.

--- unid/app/models/user.rb
+++ unid/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   has_secure_password
   has_many :profiles
 
--- unid/db/schema.rb
+++ unid/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515141844) do
+ActiveRecord::Schema.define(version: 20170515210708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20170515141844) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "temp_password"
+    t.string   "avatar"
   end
 
   add_foreign_key "profiles", "users"

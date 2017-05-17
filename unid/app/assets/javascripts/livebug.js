20ed4678c8b5237016ed6a05609b67270240686d
Sam Slotnick <samslotnick@Sams-MacBook-Pro.local>
2017-05-17 14:41:49 -0400

    got signup form to fadein

--- unid/app/assets/javascripts/livebug.js
+++ unid/app/assets/javascripts/livebug.js
@@ -1,37 +1,37 @@
-$('document').ready(function() {
-
-var headerUrl = "https://api.twitch.tv/kraken/streams/" + username;
-
-$.ajaxSetup ({
-        cache: false
-    });
-
-    var interval
-
-    function livebug() {
-
-
-  $.ajax({
-    url: headerUrl,
-    method: 'get',
-    response_type: 'jsonp',
-    data: {},
-    headers: {'Client-ID': 'uj53o98rasdwbhwic692sh6jmd36w5'}
-  }).success( function(response) {
-    console.log(response);
-    if (response['stream'] == null) {
-      $('#twitch_card').html("offline")
-    }
-    else {
-      $('#twitch_card').html("LIVE")
-    }
-  });
-};
-
-livebug();
-
-interval = setInterval(livebug, 120000);
-
-
-
-});
+// $('document').ready(function() {
+//
+// var headerUrl = "https://api.twitch.tv/kraken/streams/" + username;
+//
+// $.ajaxSetup ({
+//         cache: false
+//     });
+//
+//     var interval
+//
+//     function livebug() {
+//
+//
+//   $.ajax({
+//     url: headerUrl,
+//     method: 'get',
+//     response_type: 'jsonp',
+//     data: {},
+//     headers: {'Client-ID': 'uj53o98rasdwbhwic692sh6jmd36w5'}
+//   }).success( function(response) {
+//     console.log(response);
+//     if (response['stream'] == null) {
+//       $('#twitch_card').html("offline")
+//     }
+//     else {
+//       $('#twitch_card').html("LIVE")
+//     }
+//   });
+// };
+//
+// livebug();
+//
+// interval = setInterval(livebug, 120000);
+//
+//
+//
+// });
---
+++ unid/app/assets/javascripts/users.js
@@ -0,0 +1,9 @@
+var signupForm = $("<div></div>");
+$(document).ready(function(){
+  $("#login_link").on('click', function(e){
+    e.preventDefault();
+    $("#signUpLayout").fadeIn();
+
+
+  });
+});

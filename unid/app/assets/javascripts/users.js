6093317d405880fbb226150cf33881e5317ae799
Sam Slotnick <samslotnick@Sams-MacBook-Pro.local>
2017-05-17 15:04:34 -0400

    prevent default form submission

--- unid/app/assets/javascripts/users.js
+++ unid/app/assets/javascripts/users.js
@@ -3,7 +3,16 @@ $(document).ready(function(){
   $("#login_link").on('click', function(e){
     e.preventDefault();
     $("#signUpLayout").fadeIn();
-
-
+  });
+  $(".fadeInForm").on('submit', function (e){
+    e.preventDefault();
+    // $.ajax({
+    //   url: $(this).attr('action'),
+    //   method: $(this).attr('method'),
+    //   dataType: "html",
+    //   data: this.serialize()
+    // }).done( ).fail().always(function (data){
+    //   console.log(data);
+    // });
   });
 });

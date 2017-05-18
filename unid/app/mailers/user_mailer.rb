6d04174ff8b65a82e6e2222f12fcc0d9d5b29a41
quantumduck <quantum.duck@hotmail.com>
2017-05-18 13:59:57 -0400

    moved views to proper location

--- unid/app/mailers/user_mailer.rb
+++ unid/app/mailers/user_mailer.rb
@@ -8,15 +8,21 @@ class UserMailer < ApplicationMailer
       @name = user.username
     end
     @link = "#{root_url}#{user.username}/#{user.temp_password}/change_password"
+    mail(to: @user.email, subject: 'Thank you for signing up to our awesome site!')
   end
 
-  def reset_email
+  def reset_email(user)
     if user.name
       @name = user.name
     else
       @name = user.username
     end
     @link = "#{root_url}#{user.username}/#{user.temp_password}/change_password"
+    mail(to: @user.email, subject: 'Password Reset')
+  end
+
+  def email_change(user, old_email)
+
   end
 
 

18f6c907700e65beeb353a64ec9ba7181d0cc8bb
quantumduck <quantum.duck@hotmail.com>
2017-05-19 15:15:29 -0400

    adding heroku settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'https://serene-river-39981.herokuapp.com/' }
+  # config.action_mailer.default_url_options = { host: 'https://serene-river-39981.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {

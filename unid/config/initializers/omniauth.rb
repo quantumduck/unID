2eccd193370caf5612efdfd7abb301730eae2d28
quantumduck <quantum.duck@hotmail.com>
2017-05-19 10:21:50 -0400

    added missing comma to twitter strategy

--- unid/config/initializers/omniauth.rb
+++ unid/config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :tumblr, ENV['TUMBLR_KEY'], ENV['TUMBLR_SECRET']
-  provider :twitter, ENV["twitter_consumer_key"], ENV["twitter_consumer_secret"]
+  provider :twitter, ENV["twitter_consumer_key"], ENV["twitter_consumer_secret"],
   {
     :authorize_params => {
       :force_login => 'true',

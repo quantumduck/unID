0c68d731c494a9b503664152a7111a8a2765b9a3
quantumduck <quantum.duck@hotmail.com>
2017-05-22 12:50:54 -0400

    adjustment for picture posts in tumblr

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,12 +1,12 @@
 class BlogPost
 
-  attr_reader :provider, :text, :url
+  attr_reader :provider, :text, :url, :picture
 
   def initialize(data)
     @provider = data[:provider]
     @text = data[:text]
     @url = data[:url]
-    @picture = data[:image]
+    @picture = data[:picture]
   end
 
   def self.get_twitter(profile)
@@ -24,10 +24,26 @@ class BlogPost
   def self.get_tumblr(profile)
     response = HTTParty.get(
       "https://api.tumblr.com/v2/blog/" + \
-      "#{profile.uid}" + \
+      "#{profile.uid}" + ".tumblr.com"\
       "/posts?api_key=" + \
       "#{ENV['TUMBLR_KEY']}"
     )
+    posts =
+    response['response']['posts'].map do |post|
+      case post['type']
+      when 'photo'
+        image = post['photos'][0]['original_size']['url']
+      else
+        image = nil
+      end
+      new(
+        provider: 'tumblr',
+        text: post['summary'],
+        url: post['post_url'],
+        picture: image
+      )
+    end
+    posts
   end
 
 end

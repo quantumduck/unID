54e2cefe909aa70664eb771db8dd68c448c28e80
Alex <alex.mahinko@gmail.com>
2017-05-25 17:07:40 -0400

    Social icons work the way good links ought to.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,15 +32,29 @@ $(function() {
     revert: true,
   });
 
+  $('#faceboo_login').on('click', function(){
+    window.location.href='/auth/facebook'
+  });
+
+  $('#twitter_login').on('click', function(){
+    window.location.href='/auth/twitter'
+  });
+
+  $('#google_login').on('click', function(){
+    window.location.href='/auth/google'
+  });
+
+  $('#linkedin_login').on('click', function(){
+    window.location.href='/auth/linkedin'
+  });
+
 
 
 
 $('.toggle_search').on('click', function(){
   if ($('#search-form').css('display')==='none') {
-    console.log("CLICK");
     $("#search-form").show("slide", {direction: "left"}, 500);
   } else {
-    console.log("UNCLICK");
     $("#search-form").hide("slide", {direction: "left"}, 500);
   }
 });

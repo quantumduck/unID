49f698500ccbeddfbdc2eb48809565659e9d499a
Devin Leduc <ixeres@gmail.com>
2017-05-28 19:25:10 -0400

    Added new overlay js, implementing on vertical nav.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
-//= require scotchPanels.js
+//= require jquery.slidereveal.min
 
 
 $(function() {
@@ -58,13 +58,10 @@ $('.toggle_search').on('click', function(){
   }
 });
 
-$('#scotch-panel').scotchPanel({
-              containerSelector: '.container-fluid',
-              direction: 'right',
-              duration: 300,
-              transition: 'ease',
-              clickSelector: '.toggle-panel',
-              distanceX: '200px',
-              enableEscapeKey: true
-          });
+$('#slider').slideReveal({
+  trigger: $("#trigger"),
+  push: false,
+  width: -600,
+  autoEscape: true,
+});
           });

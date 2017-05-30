fc35e2063eb415ee897dbf278906ee051da70513
quantumduck <quantum.duck@hotmail.com>
2017-05-30 14:37:09 -0400

    fixing default image

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -20,7 +20,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-    'unidcorn-icon3'
+    'unidcorn-icon3.png'
   end
 
   # Process files as they are uploaded:
